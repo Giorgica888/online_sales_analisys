8e428cf33e18455b0f9e91beb5fa65c55cf61a42
Giorgica888 <georgeungureanugm@gmail.com>
2026-02-05 18:47:57 +0200

    Here is the last commit but without the console-printing

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from product import Product
 from product_manager import ProductManager
 
 manager = ProductManager([])
-manager.add_product(Product("Scrisors", 10, 200))
-manager.add_product(Product("Rose", 20, 80))
-manager.add_product(Product("Lilys", 21, 31))
-manager.add_product(Product("Sign with Tokyo", 24, 62))
+manager.add_product(Product("L'scrisors", 10, 200))
+manager.add_product(Product("Rose", 20, 81))
+manager.add_product(Product("Lilys and play", 21, 31))
+manager.add_product(Product("Sign with Tokyo", 30, 62))
 
-manager.show_products(pretty_printing=True)
-manager.show_price_sum()
+
