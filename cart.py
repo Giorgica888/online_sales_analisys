c2c3b7bd1179b90d7bb196519e163ea70ea4389a
Giorgica888 <georgeungureanugm@gmail.com>
2026-02-05 18:44:01 +0200

    v2.0 Added Cart functionality and added new feature for already existing product instances in the Products Manager

--- cart.py
+++ cart.py
@@ -1,20 +1,26 @@
 from product import Product
 
-class ProductManager:
+class Cart:
     def __init__(self, product_list : list[Product] = []):
         self.product_list = product_list
         self.id_set = set()
         self.id_markup() #Inserts the IDs to the set
-    
+
     def add_product(self, product : Product):#Checks and adds the product to the list, and the ID to the set
         if isinstance(product, Product) and product.id not in self.id_set:
             self.product_list.append(product)
             self.id_set.add(product.id)
         elif isinstance(product, Product) and product.id in self.id_set:
-            product.quantity += 1 
+            product.quantity += 1 #the same reference in memory
         else:
             print("Something went wrong")
     
+    def sum_of_products(self):
+        the_sum = 0
+        for i in self.product_list:
+            the_sum += i.price * i.quantity
+        return the_sum
+
     def show_products(self, pretty_printing = False):
         if pretty_printing:
             for i in self.product_list:
@@ -23,33 +29,8 @@ class ProductManager:
             for i in self.product_list:
                 print(i.quantity , "X ",i.name, end = " | ")
 
-    def show_price_sum(self):
-        the_sum = 0
-        for i in self.product_list:
-            the_sum += i.price * i.quantity
-        print(f"Total price : {the_sum}")
-
-    def remove_product(self, product_name : str):
-        for i in self.product_list:
-            if i.name == product_name:
-                self.product_list.remove(i)
-                self.id_set.remove(i.id)
-                return
-            
-        if self.product_list:
-            print("Product does not exist in the list")
-        else:
-            print("The list is empty")
-
     def id_markup(self):
         for i in self.product_list:
             self.id_set.add(i.id)
 
-#Direct Test Place  
-# maneger = ProductManager([Product("masian", 100, 10), Product("caruta", 20, 10)])
-# maneger.add_product(Product("tir", 22, 10))
-
-# maneger.show_products(pretty_printing=True)
-
-# maneger.remove_product("masian")
-# maneger.show_products(pretty_printing=True)
+testy = Cart([Product("masian", 100, 10), Product("caruta", 20, 10)])
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
-manager = ProductManager([])
+manager = ProductManager()
 manager.add_product(Product("Scrisors", 10, 200))
 manager.add_product(Product("Rose", 20, 80))
 manager.add_product(Product("Lilys", 21, 31))
@@ -9,3 +10,10 @@ manager.add_product(Product("Sign with Tokyo", 24, 62))
 
 manager.show_products(pretty_printing=True)
 manager.show_price_sum()
+
+print("\n\n\n")
+shopping_cart = Cart()
+for i in range(3):
+    shopping_cart.add_product(manager.product_list[i])
+print(f"The total pay amount is {shopping_cart.sum_of_products()}")
+shopping_cart.show_products(pretty_printing=True)
